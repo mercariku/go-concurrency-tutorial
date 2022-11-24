6ec36b9d730ced18f59cd987f35034116c480ee4
RikuKD <76926692+mercariku@users.noreply.github.com>
2022-11-25 03:35:14 +0900

    tidying up code - removing manual checks (refer to previous commit) - added notes / comments

--- cmd/main.go
+++ cmd/main.go
@@ -5,22 +5,18 @@ import (
 	"time"
 )
 
-func main() {
+func main() { // 1: main routing starts
 	c := make(chan string)
-	go count("sheep", c)
+	go count("sheep", c) // 2: second go routine starts which runs the count function
 
-	for {
-		msg, open := <-c
-		if !open {
-			break
-		}
+	for msg := range c { // 4: exists the loop once c range satisfied
 		fmt.Println(msg)
 	}
 }
 
 func count(thing string, c chan string) {
 	for i := 1; i <= 5; i++ {
-		c <- thing
+		c <- thing // 3: send value through channel created in line 9 (remember that sending through channel is a blocking operation)
 		time.Sleep(time.Millisecond * 500)
 	}
 
