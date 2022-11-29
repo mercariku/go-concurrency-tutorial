b25fb190a32f870f4810adfd841d904e0a7462c8
RikuKD <76926692+mercariku@users.noreply.github.com>
2022-11-29 17:01:16 +0900

    concurrency pattern: select case wrapped in for loop

--- kantan-coding/primitives.go
+++ kantan-coding/primitives.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -10,31 +11,24 @@ func someFunc(num string) {
 }
 
 func main() {
-	c := make(chan string)
-	d := make(chan string)
-	go someFunc("1") // buffered channel is asynchronous (this means the sending go routine can send and forget) - meanwhile, unbuffered channel is synchronous because sending and receiving take place in synch.
-	go func() {
-		c <- "test" // put string data onto the channel, c
-	}()
+	start := time.Now()
 
-	go func() {
-		d <- "data"
-	}()
+	charChannel := make(chan string, 3)
+	chars := []string{"a", "b", "c"}
 
-	time.Sleep(time.Second * 2)
-	msg := <-c
-	fmt.Println(msg)
-	fmt.Println("hi")
-
-	select {
-	case msgOne := <-c:
-		fmt.Println(msgOne)
-	case msgTwo := <-d:
-		fmt.Println(msgTwo)
+	for _, s := range chars {
+		select {
+		case charChannel <- s:
+		}
 	}
 
-}
+	close(charChannel)
 
-// unbuffered channel is synchronous because the sending side is blocked until there's a response from the receiver
-// buffered channel is asynchronous because sending go routine can send data to the buffer queue (until it's full). The sending go routine can keep processing workload without waiting for response by a receiver
-// the sending go routine is blocked once the queue is full
+	for result := range charChannel {
+		fmt.Println(result)
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("%s", elapsed)
+
+}
